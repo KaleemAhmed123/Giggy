8206c7a24fff1f933b7d96ab921e0287e329842a
KaleemAhmed123 <shaamidreez@gmail.com>
2023-03-06 17:06:38 +0530

    Initial commit (setup done).

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-function App() {
+import React from "react";
 
-  return (
-    <div>
-      Hello world!
-    </div>
-  )
-}
+const App = () => {
+  return <div>Hello World</div>;
+};
 
-export default App
+export default App;
