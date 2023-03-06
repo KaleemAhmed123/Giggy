9dda921a37dc30fde5106707cdd55eb70d1c6413
KaleemAhmed123 <shaamidreez@gmail.com>
2023-03-06 17:32:52 +0530

    All Pages added and index file done.

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Navbar from "./navbar/Navbar";
+import Featured from "./featured/Featured";
+import CatCard from "./catCard/CatCard";
+import Slide from "./slide/Slide";
+import GigCard from "./gigCard/GigCard";
+import ProjectCard from "./projectCard/ProjectCard";
+import TrustedBy from "./trustedBy/TrustedBy";
+import Footer from "./footer/Footer";
+
+export {
+  Navbar,
+  Featured,
+  CatCard,
+  Slide,
+  GigCard,
+  ProjectCard,
+  TrustedBy,
+  Footer,
+};
