a73392d7ed5efe8329294661444047914eed916c
KaleemAhmed123 <shaamidreez@gmail.com>
2023-03-07 15:13:45 +0530

    Router setup done and upper nav done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,82 @@
 import React from "react";
+import "./app.scss";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
-const App = () => {
-  return <div>Hello World</div>;
-};
+import { Navbar, Footer } from "./components";
+import {
+  Home,
+  Gigs,
+  Gig,
+  Login,
+  Register,
+  Add,
+  Orders,
+  Messages,
+  Message,
+  MyGigs,
+} from "./pages";
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+  //takes array of pth objs
+  const router = createBrowserRouter([
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/myGigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,138 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./Navbar.scss";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+function Navbar() {
+  const downMenuItems = [
+    "Graphics & Design",
+    "Video & Animation",
+    "Writing & Translation",
+    "AI Services",
+    "Digital Marketing",
+    "Music & Audio",
+    "Programming & Tech",
+    "Business",
+    "Lifestyle",
+  ];
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const { pathname } = useLocation();
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  // const currentUser = null
+
+  const currentUser = {
+    id: 1,
+    username: "Anna",
+    isSeller: true,
+  };
+
+  return (
+    <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          <Link className="link" to="/">
+            <span className="text">liverr</span>
+          </Link>
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Liverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {currentUser ? (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img
+                src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>{currentUser?.username}</span>
+              {/* that open option menu on profile (abs pos right-down do profile icon) */}
+              {open && (
+                <div className="options">
+                  {currentUser.isSeller && (
+                    <>
+                      <Link className="link" to="/mygigs">
+                        Gigs
+                      </Link>
+                      <Link className="link" to="/add">
+                        Add New Gig
+                      </Link>
+                    </>
+                  )}
+                  {/* if not seller only order message logout */}
+                  <Link className="link" to="/orders">
+                    Orders
+                  </Link>
+                  <Link className="link" to="/messages">
+                    Messages
+                  </Link>
+                  <Link className="link" to="/">
+                    Logout
+                  </Link>
+                </div>
+              )}
+            </div>
+          ) : (
+            <>
+              <span>Sign in</span>
+              <Link className="link" to="/register">
+                <button>Join</button>
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+      {(active || pathname !== "/") && (
+        <>
+          <hr />
+          {/* TODO- Loop through array to reduce code */}
+          <div className="menu">
+            <Link className="link menuLink" to="/">
+              Graphics & Design
+            </Link>
+            <Link className="link menuLink" to="/">
+              Video & Animation
+            </Link>
+            <Link className="link menuLink" to="/">
+              Writing & Translation
+            </Link>
+            <Link className="link menuLink" to="/">
+              AI Services
+            </Link>
+            <Link className="link menuLink" to="/">
+              Digital Marketing
+            </Link>
+            <Link className="link menuLink" to="/">
+              Music & Audio
+            </Link>
+            <Link className="link menuLink" to="/">
+              Programming & Tech
+            </Link>
+            <Link className="link menuLink" to="/">
+              Business
+            </Link>
+            <Link className="link menuLink" to="/">
+              Lifestyle
+            </Link>
+          </div>
+          <hr />
+        </>
+      )}
+    </div>
+  );
+}
 
 export default Navbar;
