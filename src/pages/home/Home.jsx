7999418b6352607b9597ca75f3d5a2ebda8218d4
KaleemAhmed123 <shaamidreez@gmail.com>
2023-03-10 12:30:05 +0530

    upper Scroll done.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slide/Slide";
+import CatCard from "../../components/catCard/CatCard";
+import { cards } from "../../data";
 import "./Home.scss";
 const Home = () => {
   return (
     <div className="home">
       <Featured />
       <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
+        ))}
+      </Slide>
     </div>
   );
 };
