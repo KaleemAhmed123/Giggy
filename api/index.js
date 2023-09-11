52de41dfe473c77071517e7d538c4251574ead4a
KaleemAhmed123 <shaamidreez@gmail.com>
2023-09-11 15:26:51 +0530

    Basics setup done for API.

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+mongoose.set("strictQuery", true);
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDB!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.listen(8800, () => {
+  connect();
+  console.log("Backend server is running!");
+});
