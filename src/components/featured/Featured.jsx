c17e2b82953880e19e93bcd7bb9d6dd7db80daf7
KaleemAhmed123 <shaamidreez@gmail.com>
2023-09-15 12:42:16 +0530

    Search on feature and tags done.

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,6 +1,31 @@
 import React from "react";
 import "./Featured.scss";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Featured = () => {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  // const handleSearch = (e) => {
+  //   if (e.type === "click" || (e.type === "keypress" && e.key === "Enter")) {
+  //     navigate(`/gigs?search=${input}`);
+  //   }
+  // };
+  const handleSearch = () => {
+    navigate(`/gigs?search=${input}`);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      // Trigger the search when the "Enter" key is pressed
+      handleSearch();
+    }
+  };
+
+  const handleTags = (e) => {
+    navigate(`/gigs?cat=${e.target.value}`);
+  };
+
   return (
     <div className="featured">
       <div className="container">
@@ -11,16 +36,29 @@ const Featured = () => {
           <div className="search">
             <div className="searchInput">
               <img src="./img/search.png" alt="" />
-              <input type="text" placeholder='Try "building mobile app"' />
+              <input
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={handleKeyPress}
+                type="text"
+                placeholder='Try "building mobile app"'
+              />
             </div>
-            <button>Search</button>
+            <button onClick={handleSearch}>Search</button>
           </div>
           <div className="popular">
             <span>Popular:</span>
-            <button>Web Design</button>
-            <button>WordPress</button>
-            <button>Logo Design</button>
-            <button>AI Services</button>
+            <button onClick={handleTags} value="coding">
+              Coding
+            </button>
+            <button onClick={handleTags} value="Javascript">
+              Javascript
+            </button>
+            <button onClick={handleTags} value="design">
+              Design
+            </button>
+            <button onClick={handleTags} value="ai">
+              AIML
+            </button>
           </div>
         </div>
 
