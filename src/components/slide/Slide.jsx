a5f80979550dff15bf6f21808379605ae6357b4c
KaleemAhmed123 <shaamidreez@gmail.com>
2023-09-17 21:55:05 +0530

    some responsiveness added as of now.

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Slide.scss";
 import Slider from "infinite-react-carousel";
-// passing corousel data dynamically
-const Slide = ({ children, slidesToShow, arrowsScroll }) => {
+
+const Slide = ({ children }) => {
+  const [slidesToShow, setSlidesToShow] = useState(3); // Default to show 3 slides
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setSlidesToShow(4); // Show 3 slides for screens wider than 1024px
+      } else if (window.innerWidth >= 780) {
+        setSlidesToShow(3); // Show 2 slides for screens wider than 780px
+      } else if (window.innerWidth >= 420) {
+        setSlidesToShow(2); // Show 2 slides for screens wider than
+      } else {
+        setSlidesToShow(1); // Show 1 slide for screens narrower than 780px
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="slide">
       <div className="container">
-        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
+        <Slider slidesToShow={slidesToShow} centerPadding={0} adaptiveHeight>
           {children}
         </Slider>
       </div>
