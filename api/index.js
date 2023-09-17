bd028cd20fbbf5b87f376cdc57ad94d46e2cf553
KaleemAhmed123 <shaamidreez@gmail.com>
2023-09-17 22:36:50 +0530

    cors fixed.

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: "https://giggy-api.onrender.com", credentials: true }));
+app.use(cors({ origin: "https://giggy.netlify.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
--- src/utils/axiosUtil.js
+++ src/utils/axiosUtil.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://giggy-api.onrender.com/api/",
+  baseURL: "https://giggy-api.onrender.com",
   withCredentials: true,
 });
 
